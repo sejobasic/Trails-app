a30de5680e10fc4e0a8cb754c54bbd7024a728fc
Jerry Tong <zihangtong@gmail.com>
2022-02-17 13:33:53 -0500

    Carousel

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,51 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import TrailCards from './TrailCards'
 import { Button, Col, Card, Row, Container, Carousel } from 'react-bootstrap';
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function Home({ trails }) {
 
+  const mappedTrails = trails.map(trail => {
+    const city = trail.city
+    const state = trail.state
+    const name = trail.name.split(' ').join('');
 
+    return (
+      <Carousel.Item key={trail.id} as={Link} to={`/trails/${state}/${city}/${name}`}>
+        <img
+          src={trail.image_url}
+          alt={`${trail.name} Slide`}
+        />
+        <Carousel.Caption>
+          <h3>{trail.name}</h3>
+          <p>{trail.city}, {trail.state}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    )
+  })
   
   return (
     <div>
-        <Carousel className="carousel-container" >
-          <Carousel.Item className="img-overlay">
-            <img
-              className="d-block w-100"
-              src="https://cdn.postindependent.com/wp-content/uploads/sites/6/2016/06/RifleArch-RCT-062614-2-1-1240x930.jpg"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>Rifle Arch Trail</h3>
-              <p>Rifle, Colorado </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="img-overlay">
-            <img
-              className="d-block w-100"
-              src="https://2.bp.blogspot.com/-Yu_udbyPrPA/T7a1MbksOUI/AAAAAAAAJdo/7keEtyCMrqM/s1600/Rifle%2B446.JPG"
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Rifle Falls Coyote to Bobcat Trails</h3>
-              <p>Silt, Colorado</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="img-overlay">
-            <img
-              className="d-block w-100"
-              src="https://www.imagesfromcolorado.com/images/xl/Grand-Mesa-Scenic-Byway-7-1.jpg"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Grand Mesa Scenic Byway</h3>
-              <p>Mesa Lakes, Colorado</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+      <Carousel className="carousel-container" >
+        {mappedTrails}
+      </Carousel>
 
       
       {trails.map((trail) => {
