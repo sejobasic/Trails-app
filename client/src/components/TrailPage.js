140c33e678df4757a66f7ab456a7a016e4d55dda
Jerry Tong <zihangtong@gmail.com>
2022-02-18 00:22:52 -0500

    google maps

--- client/src/components/TrailPage.js
+++ client/src/components/TrailPage.js
@@ -64,10 +64,16 @@ function TrailPage({ trailId }) {
     })
   }
 
+
+
 if (!loaded) {
   return <div>loading...</div>
 }
+
+const mapURL = `https://www.google.com/maps/embed/v1/place?key=AIzaSyAnKa-88F4rgDA_-8giFeehPgxKsEQSPzk&q=${trail.name},${trail.city},${trail.state}`
+
   return (
+    
     <>
     <div>
       {/* <TrailCards trail={trail}/> */}
@@ -84,6 +90,9 @@ if (!loaded) {
           </Card.ImgOverlay>
         </Card>
       </Container>
+
+
+      <iframe src={mapURL} width="100%" height="300" style={{border:0}} allowfullscreen></iframe>
 
 
       {reviews.map(review => {
